57b2f5cde4475eb4b5a1cc3eff154a5d03d14b18
Shekhar <up81.shekhar@gmail.com>
2026-08-27 09:36:10 +0530

    Revert RoadmapDetail import to AdSlot

--- client/src/routes/RoadmapDetail.tsx
+++ client/src/routes/RoadmapDetail.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { Link, useParams } from "react-router-dom";
 import SkillTree from "../components/roadmap/SkillTree";
-import AdsterraSlot from "../components/ads/AdsterraSlot";
+import AdSlot from "../components/ads/AdSlot";
 import { LineColor } from "../data/sampleRoadmaps";
 import { useAdminStore } from "../store/adminStore";
 import { useProgressStore } from "../store/progressStore";

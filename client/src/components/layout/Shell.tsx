76f6bd6ac2ac4f77957214e594adf1eaf23b54e2
Shekhar <up81.shekhar@gmail.com>
2026-09-08 11:10:56 +0530

    Mount BadgeToast in Shell

--- client/src/components/layout/Shell.tsx
+++ client/src/components/layout/Shell.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import BadgeToast from "../BadgeToast";
 
 export default function Shell({ children }: { children: ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function Shell({ children }: { children: ReactNode }) {
       <Nav />
       <main className="flex-1">{children}</main>
       <Footer />
+      <BadgeToast />
     </div>
   );
 }
